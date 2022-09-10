17a7223ff9ca1141368be56d198c1fb45d57fb33
Aoufi Abderhmane <Abdouou7@gmail.com>
2022-09-10 14:29:55 +0100

    add book controllers

---
+++ src/controllers/book.js
@@ -0,0 +1,15 @@
+export const createBook = (req, res, _next) => {
+  res.json({ message: "not implemented!" });
+};
+
+export const readBook = (req, res, _next) => {
+  res.json({ message: "not implemented!" });
+};
+
+export const updateBook = () => {
+  res.json({ message: "not implemented" });
+};
+
+export const deleteBook = () => {
+  res.json({ message: "not implemented" });
+};
